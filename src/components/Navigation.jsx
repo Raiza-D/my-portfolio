ab2804b1a0ca3eaeed9c2c1e8af81a1aca715f98
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-06 21:55:43 -0800

    Add categories array of objects for nav menu items. Add my name and map through categories array to display nav menu items.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 
 export default function Navigation() {
+
+  const categories = [
+    {
+      name: "Home",
+      href: "#home",
+      id: 1,
+    },
+    {
+      name: "About",
+      href: "#about",
+      id: 2,
+    },
+    {
+      name: "Skills",
+      href: "#skills",
+      id: 3,
+    },
+    {
+      name: "Projects",
+      href: "#projects",
+      id: 4,
+    },
+    {
+      name: "Contact",
+      href: "#contact",
+      id: 5,
+    },
+  ];
+
+
   return (
     <div>
+      <nav>
+        <span>Raiza de Guzman</span>
+        <span>
+          {categories.map((category) => {
+            return (
+              <a key={category.id} href={category.href}>
+                {category.name}
+              </a>
+            );
+          })}
+        </span>
+      </nav>
     </div>
   );
 }
