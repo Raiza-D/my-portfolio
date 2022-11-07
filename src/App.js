bfd1dc2ead1db3fde6e5585c98587254bea5de00
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-06 20:08:17 -0800

    Import newly created component files. Add components as children, to be rendered

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
