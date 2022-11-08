d076259faea007c9c741f1c9ebf33293a9efc392
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-07 23:49:53 -0800

    Import Container, Row, Col react bootstrap components. Import css file for About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import "../styles/About.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function About() {
   return (
-    <div>
+    <div className="about">
+      <Container>
+        <Row>About Me</Row>
+        <Row></Row>
+        <Row></Row>
+      </Container>
     </div>
   );
 }
