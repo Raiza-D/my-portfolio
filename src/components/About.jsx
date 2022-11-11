5d6d161f2376e882a13170fecbfecb2daa13938c
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-10 23:19:51 -0800

    Remove imports for bootstrap elements. Elements removed from return statement

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import "../styles/About.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+
 
 export default function About() {
   return (
     <div className="about">
-      <Container>
-        <Row>About Me</Row>
-        <Row></Row>
-        <Row></Row>
-      </Container>
+
     </div>
   );
 }
