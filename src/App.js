63df104d89c97e657c01a1a27cfad11683724d65
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-13 21:18:33 -0800

    Add class 'App' and Import App.css file. Styling for App parent component will go in css file

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Navigation />
       <Home />
       <About />
