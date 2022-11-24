ff427fb0e522f1092d3fadb4b5d666f0c94f2069
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-23 21:45:42 -0800

    Import stylesheet for Home component. Add content to Home section and apply styling & layout. Import background img from assets sub dir.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import homeBg from "../assets/programming-wp.jpg";
+import "../styles/Home.css";
 
 export default function Home() {
   return (
-    <div>
-      <img src={homeBg} alt="orange-black-wp" className="w-100"/>
+    <div className="home d-flex justify-content-center">
+      <div>
+        <p className="p-square-brack">[ ]</p>
+      </div>
+      <div>
+        <p className="p-my-name">Raiza De Guzman_</p>
+        <p className="p-my-title">Full-Stack Web Developer</p>
+      </div>
+      {/* <img src={homeBg} alt="orange-black-wp" className="w-100"/> */}
     </div>
   );
 }
