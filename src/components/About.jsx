fede91ff714aab8280bd8d9614e8197a38f3c6df
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-23 22:15:57 -0800

    Add p tag to contain info about me and placeholder text.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,6 +7,13 @@ export default function About() {
     <div className="about">
       <Container>
         <h3>About Me</h3>
+        <p>
+          Donec at tempus lectus, vitae dictum augue. Sed rhoncus tellus sed
+          facilisis fermentum. Duis cursus luctus sapien vitae tempor.
+          Vestibulum tempor suscipit aliquet. Ut iaculis felis mauris, ut ornare
+          risus rhoncus eu. Proin sem mauris, faucibus vitae lobortis ac, congue
+          ac nibh. Maecenas eget iaculis sem.{" "}
+        </p>
       </Container>
     </div>
   );
