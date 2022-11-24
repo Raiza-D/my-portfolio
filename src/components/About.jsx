dc006d7a44cce7477944259d03c03a49baa316fc
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-23 23:05:02 -0800

    Add write-up for brief info about myself.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,11 +10,13 @@ export default function About() {
         <h3 className="pb-3">About Me</h3>
         <img src={myPic} alt="raiza-headshot" />
         <p>
-          Donec at tempus lectus, vitae dictum augue. Sed rhoncus tellus sed
-          facilisis fermentum. Duis cursus luctus sapien vitae tempor.
-          Vestibulum tempor suscipit aliquet. Ut iaculis felis mauris, ut ornare
-          risus rhoncus eu. Proin sem mauris, faucibus vitae lobortis ac, congue
-          ac nibh. Maecenas eget iaculis sem.
+          Hello there, Visitor! I'm Raiza, nice to e-meet you. I turned my love of resolving inventory variances into solving coding challenges when I re-discovered coding in 2021 and decided to pursue it as a career.
+        </p>
+        <p>
+          I have over eight years of experience working in a team-focused and client-oriented environment, working closely with people from different departments and across varying reporting levels within an organization. I'm excited to launch my new career in tech and utilize my invaluable experiences in a Web Development capacity. My preference is the Front-End, as I love the visuals, but I'm equally eager to apply my skills and continue learning in a Full-Stack role.
+        </p>
+        <p>
+          When I'm not indoors crafting code, I can be found hiking in nature and happily standing on top of a long-awaited mountain peak beaming my signature, exceptionally-friendly smile.
         </p>
       </Container>
     </div>
