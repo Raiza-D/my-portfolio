c1dc6bc3a23f0d42f624071a24db86e75bef6d18
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-23 22:27:38 -0800

    Import my cropped headshot. Add img tag and reference my headshot pic.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../styles/About.css";
 import Container from "react-bootstrap/Container";
+import myPic from "../assets/rdg6426-cropped.png";
 
 export default function About() {
   return (
     <div className="about">
       <Container className="pt-3 pb-3">
         <h3>About Me</h3>
+        <img src={myPic} alt="raiza-headshot" />
         <p>
           Donec at tempus lectus, vitae dictum augue. Sed rhoncus tellus sed
           facilisis fermentum. Duis cursus luctus sapien vitae tempor.
