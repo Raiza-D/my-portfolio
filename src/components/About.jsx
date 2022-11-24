065d8ee3599c5710dca74567e651dabc4976c6f0
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-23 22:14:37 -0800

    Import Container bootstrap element. Wrap 'About Me' header in h3. Wrap h3 in Container elem.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/About.css";
-
+import Container from "react-bootstrap/Container";
 
 export default function About() {
   return (
     <div className="about">
-      About Me
+      <Container>
+        <h3>About Me</h3>
+      </Container>
     </div>
   );
 }
