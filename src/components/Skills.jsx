9525bc2055996aaad0c7bf0356e799599ba4ff58
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-28 21:49:30 -0800

    Add bootstrap Card component. Will display each individual skill with a technology or tool.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,6 +8,16 @@ export default function Skills() {
     <div className="skills">
       <Container>
         <h3 className="pt-3 pb-3">Skills</h3>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </Container>
     </div>
   );
