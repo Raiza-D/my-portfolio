f5bdd8b4e56fb5d2bd98c5ccf2e94175366a0542
Raiza De Guzman <santos_dg7@hotmail.com>
2022-11-28 21:37:18 -0800

    Add class called 'skills' to apply styling specified in Skills stylesheet.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 
 export default function Skills() {
   return (
-    <div>
+    <div className="skills">
       <Container>
         Skills
       </Container>
