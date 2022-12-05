29ed5d0a4ec2bb8729e4d806e2ab70d2d0b257e6
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-04 23:56:48 -0800

    Import html.png file from assets/skills subdir. Add 'card' class. Set src attrib value to html.png file path.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import "../styles/Skills.css";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
+import "../assets/skills/html.png";
 
 export default function Skills() {
   return (
     <div className="skills pb-3">
       <Container>
         <h3 className="pt-3 pb-3">Skills</h3>
-        <Card style={{ width: "13rem" }}>
-          <Card.Img variant="top" src="" />
+        <Card style={{ width: "13rem" }} className="card">
+          <Card.Img variant="top" src="../assets/skills/html.png" />
           <Card.Body>
             <Card.Title className="text-center">Card Title</Card.Title>
           </Card.Body>
