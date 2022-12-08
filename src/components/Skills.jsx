d9c2de3b25cc14bc1ae0617ba85753e19d038f5f
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-07 16:58:22 -0800

    Store html logo file as a variable when Importing. Comment out Card.IMG comp. Cahnge width of card. Add img tag making use of HTML variable containing logo, set width. Center Card.Body contents to center.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import "../styles/Skills.css";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
-import "../assets/skills/html.png";
+import HTML from "../assets/skills/html.png";
 
 export default function Skills() {
   return (
     <div className="skills pb-3">
       <Container>
         <h3 className="pt-3 pb-3">Skills</h3>
-        <Card style={{ width: "13rem" }} className="card">
-          <Card.Img variant="top" src="../assets/skills/html.png" />
-          <Card.Body>
-            <Card.Title className="text-center">Card Title</Card.Title>
+        <Card bg="dark" style={{ width: "10rem" }} className="card">
+          {/* <Card.Img variant="top" src="../assets/skills/html.png" /> */}
+          <Card.Body className="text-center">
+            <img src={HTML} width="40"/>
+            <Card.Title className="text-center">HTML</Card.Title>
           </Card.Body>
         </Card>
       </Container>
