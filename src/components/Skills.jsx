3f772d79f25be64a0df76d4f716a3eb95bf6906f
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-08 19:18:17 -0800

    Remove img tag and Card.Img element. Remove import for HTML logo.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "../styles/Skills.css";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
-import HTML from "../assets/skills/html.png";
 
 export default function Skills() {
   return (
@@ -10,9 +9,7 @@ export default function Skills() {
       <Container>
         <h3 className="pt-3 pb-3">Skills</h3>
         <Card bg="dark" style={{ width: "10rem" }} className="card">
-          {/* <Card.Img variant="top" src="../assets/skills/html.png" /> */}
           <Card.Body className="text-center">
-            <img src={HTML} width="40" alt="html-logo"/>
             <Card.Title className="text-center">HTML</Card.Title>
           </Card.Body>
         </Card>
