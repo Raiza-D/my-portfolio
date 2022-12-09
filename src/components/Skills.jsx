0777fa01f8698ec6163dcac55575b46dfbb853f8
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-08 19:19:04 -0800

    Remove Card bootstrap elements and its contents. Remove line to import Card component top of file.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import "../styles/Skills.css";
 import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card";
 
 export default function Skills() {
   return (
     <div className="skills pb-3">
       <Container>
         <h3 className="pt-3 pb-3">Skills</h3>
-        <Card bg="dark" style={{ width: "10rem" }} className="card">
-          <Card.Body className="text-center">
-            <Card.Title className="text-center">HTML</Card.Title>
-          </Card.Body>
-        </Card>
+
       </Container>
     </div>
   );
