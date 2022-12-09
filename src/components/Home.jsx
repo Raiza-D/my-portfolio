89f40b396a5ac3a59caa305b5fe82983cf67cc66
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-09 00:55:02 -0800

    Add id attrib with value of home to parent div of return statement.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import "../styles/Home.css";
 
 export default function Home() {
   return (
-    <div className="home d-flex justify-content-center">
+    <div id="home" className="home d-flex justify-content-center">
       <div>
         <p className="p-square-brack">[ ]</p>
       </div>
