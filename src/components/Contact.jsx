94b283d481d9ab1094eed25e6df340bf1ab15783
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-13 21:40:12 -0800

    Import Contact stylsheet. Add contact class to parent div.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import "../styles/Contact.css";
 
 export default function Contact() {
   return (
-    <div id="contact">
+    <div id="contact" className="contact">
       <Container className="pt-3">
         <h3 className="pb-3">Contact</h3>
         <p>Connect with me on these platforms:</p>
