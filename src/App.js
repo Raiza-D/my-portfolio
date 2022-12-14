6e18211d5ce0d8638ee89c9717feb3425899eaaa
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-13 20:32:59 -0800

    Import Footer component from components subdir. Add Footer child component within App parent component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Contact";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
