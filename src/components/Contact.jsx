0786577ebd7a19ea28b826901e9f3f5d3146d1a3
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-13 22:00:40 -0800

    Import LinkedIn and GitHub logos. Place them inside img tags.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import "../styles/Contact.css";
+import linkedInLogo from "../assets/linkedin-original-wordmark.svg";
+import gitHubLogo from "../assets/github-original-wordmark.svg";
 
 export default function Contact() {
   return (
@@ -8,6 +10,8 @@ export default function Contact() {
       <Container className="pt-3">
         <h3 className="pb-3">Contact</h3>
         <p>Connect with me on these platforms:</p>
+        <img src={linkedInLogo} alt="LinkedIn logo"width="8%" />
+        <img src={gitHubLogo} alt="LinkedIn logo" width="5%" />
       </Container>
     </div>
   );
