7c1e8991632b8687d3483e087cdffd59bf090165
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-13 21:04:29 -0800

    Add top padding to Container tag. Add bottom padding to h3 tag.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,8 @@ import Container from "react-bootstrap/Container";
 export default function Contact() {
   return (
     <div id="contact">
-      <Container>
-        <h3>Contact</h3>
+      <Container className="pt-3">
+        <h3 className="pb-3">Contact</h3>
         <p>Connect with me on these platforms:</p>
       </Container>
     </div>
