7bbdff40bf737b4f4439f32627ed6c7d7293987d
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-13 20:33:51 -0800

    Fix import code for Footer. Referenced the wrong file.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./components/Contact";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
