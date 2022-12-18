a4b6e9c5ae50fd5e820c79df097470d82ea13ba8
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-17 20:54:02 -0800

    Declare new projects var containing array of objects listing each project and info to display. Add new div within Container component and map through each project in projects array. Use Card and contents.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,22 +10,53 @@ import "../styles/Projects.css";
 
 
 export default function Projects() {
+
+  const projects = [
+    {
+      id: 1,
+      name: "Gardnr",
+      photo: gardnrScreenshot,
+      link: "https://github.com/Raiza-D/GARDNR-Final-Project",
+      description:
+        "A multi-page React application integrated with a weather API allowing users to manage their garden plants' watering schedules.",
+    },
+    {
+      id: 2,
+      name: "Jungle Rails",
+      photo: jungleRailsScreenshot,
+      link: "https://github.com/Raiza-D/jungle-rails",
+      description: "An e-commerce Rails app built for learniing Rails through example."
+    },
+  ];
+
+
+
   return (
     <div id="projects" className="pb-5 projects-section">
       <Container>
         <h3 className="pt-3">Projects</h3>
+        <div>
+          {projects.map(project => {
+            return (
+              <Card
+                bg="light"
+                text="dark"
+                style={{ width: "18rem" }}
+                className="text-center"
+              >
+                <Card.Img variant="top" src={project.photo} />
+                <Card.Body>
+                  <Card.Title>{project.name}</Card.Title>
+                  <Card.Text>{project.description}</Card.Text>
+                  <a href={project.link} target="_blank" rel="noreferrer">
+                    <Button variant="primary">Learn more</Button>
+                  </a>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </div>
       </Container>
-
-      <Card bg="light" text="dark" style={{ width: "18rem" }} className="text-center">
-        <Card.Img variant="top" src={gardnrScreenshot} />
-        <Card.Body>
-          <Card.Title>Gardnr</Card.Title>
-          <Card.Text>
-            A multi-page React application integrated with a weather API allowing users to manage their garden plants' watering schedules.
-          </Card.Text>
-          <Button variant="primary">Learn more</Button>
-        </Card.Body>
-      </Card>
     </div>
   );
 }
