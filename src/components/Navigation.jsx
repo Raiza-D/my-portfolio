3026cc17424cdc2ee0f29129cc5c5c0d51cb1c4c
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-17 16:22:56 -0800

    Make my name/logo clickable by wrapping img tag inside a Nav.Link bootstrap component.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,9 +7,14 @@ export default function Navigation() {
 
   return (
     <div className="menu">
-      <Navbar className="d-flex justify-content-between ps-3 pe-3" bg="myOrange">
+      <Navbar
+        className="d-flex justify-content-between ps-3 pe-3"
+        bg="myOrange"
+      >
         <Navbar.Brand>
-          <img src={myLogo} alt="rdg" width="100px" height="40px" />
+          <Nav.Link href="#home">
+            <img src={myLogo} alt="rdg" width="100px" height="40px" />
+          </Nav.Link>
         </Navbar.Brand>
         <Nav>
           <Nav.Link href="#home">Home</Nav.Link>
