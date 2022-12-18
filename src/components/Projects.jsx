164e4b8d23b0a39bf8b79b508e442067cc09b8be
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-17 19:42:34 -0800

    Remove Carousel and its contents.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,58 +12,7 @@ export default function Projects() {
     <div id="projects">
       <Container>
         <h3 className="pt-3">Projects</h3>
-        <Carousel variant="dark">
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={gardnrScreenshot}
-              alt="Gardnr screenshot"
-            />
-            <Carousel.Caption>
-              <h5>Gardnr</h5>
-              <p>
-                A React application integrated with a weather API allowing users
-                to manage their garden plants' unique watering schedules.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={jungleRailsScreenshot}
-              alt="Jungle Rails screenshot"
-            />
-            <Carousel.Caption>
-              <h5>Jungle Rails</h5>
-              <p>An e-commerce plant app built with Ruby on Rails.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={schedulerScreenshot}
-              alt="Scheduler screenshot"
-            />
-            <Carousel.Caption>
-              <h5>Scheduler</h5>
-              <p>
-                A single-page React app allowing users to book, edit, and cancel
-                interviews.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={findersSneakersScreenshot}
-              alt="FindersSneakers screenshot"
-            />
-            <Carousel.Caption>
-              <h5>FindersSneakers</h5>
-              <p>A multi-page buy/sell app for sneaker lovers!</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+
       </Container>
     </div>
   );
