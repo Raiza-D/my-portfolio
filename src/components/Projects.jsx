86b1f8fe0f1e4e259a73ac1138a80bb6f839571e
Raiza De Guzman <santos_dg7@hotmail.com>
2022-12-27 21:20:34 -0800

    Wrap Built with text in a p tag and apply bootstrap bold font weight styling.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -80,7 +80,9 @@ export default function Projects() {
                   <Card.Title>{project.name}</Card.Title>
                   <hr className="hr-projects" />
                   <Card.Text>{project.description}</Card.Text>
-                  <Card.Text className="fw-bold">Built with: </Card.Text>
+                  <Card.Text>
+                    <p className="fw-bold">Built with: </p>
+                  </Card.Text>
                   <a href={project.link} target="_blank" rel="noreferrer">
                     <Button className="rounded-0">Learn more</Button>
                   </a>
