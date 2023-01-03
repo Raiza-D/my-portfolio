f8505325d1c2db1f0f753ab0cbdf2b6260fff0f1
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-02 18:24:08 -0800

    Import Typewriter library. Add Typewriter component with options specified. Wrap Typewriter component into a p tag and apply styling.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/Home.css";
 
+import Typewriter from "typewriter-effect";
+
 export default function Home() {
   return (
     <div id="home" className="home-section d-flex justify-content-center ps-3 pe-3">
@@ -10,6 +12,17 @@ export default function Home() {
       <div>
         <p className="p-my-name">Raiza De Guzman</p>
         <p className="p-my-title">Full-Stack Web Developer_</p>
+        <p className="p-my-title">
+          <Typewriter options={{
+            strings: ["Full-Stack Web Developer", "Hello and welcome"],
+            autoStart: true,
+            loop: true,
+            delay: 90,
+            
+          }}
+          />
+        </p>
+          
       </div>
     </div>
   );
