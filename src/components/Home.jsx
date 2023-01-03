aa809898a171539f81feea60766455face6499be
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-02 18:39:19 -0800

    Comment out code we don't need to see how page looks.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "../styles/Home.css";
-
 import Typewriter from "typewriter-effect";
+
 
 export default function Home() {
   return (
     <div id="home" className="home-section d-flex justify-content-center ps-3 pe-3">
-      <div>
+      {/* <div>
         <p className="p-square-brack">[ ]</p>
-      </div>
+      </div> */}
       <div>
         <p className="p-my-name">Raiza De Guzman</p>
-        <p className="p-my-title">Full-Stack Web Developer_</p>
+        {/* <p className="p-my-title">Full-Stack Web Developer_</p> */}
         <p className="p-my-title">
           <Typewriter options={{
             strings: ["Full-Stack Web Developer", "Hello and welcome"],
