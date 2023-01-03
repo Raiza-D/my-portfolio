98daf133d01eff45106b1a18da2bd59be5519794
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-02 18:51:53 -0800

    Add home-img class to apply layout rules to computer icon img.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import computerIcon from "../assets/computer-icon.png";
 export default function Home() {
   return (
     <div id="home" className="home-section d-flex justify-content-center ps-3 pe-3">
-      <img src={computerIcon} alt="Computer icon" width="200" height="200" />
+      <img src={computerIcon} alt="Computer icon" width="200" height="200" className="home-img" />
       <div>
         <p className="p-my-name">Raiza De Guzman</p>
         <p className="p-my-title">
