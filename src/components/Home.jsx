d391e87f0762582d9a40845a564585af697a4a87
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-02 18:49:48 -0800

    Import computer icon img from assets sub-dir. Add img tag and specify its attributes.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../styles/Home.css";
 import Typewriter from "typewriter-effect";
-
+import computerIcon from "../assets/computer-icon.png";
 
 export default function Home() {
   return (
     <div id="home" className="home-section d-flex justify-content-center ps-3 pe-3">
+      <img src={computerIcon} alt="Computer icon" width="200" height="200" />
       <div>
         <p className="p-my-name">Raiza De Guzman</p>
         <p className="p-my-title">
