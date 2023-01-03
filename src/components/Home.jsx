c69473d987a05d1832f940a1c555039615b05575
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-02 18:39:48 -0800

    Remove commented out code.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,8 @@ import Typewriter from "typewriter-effect";
 export default function Home() {
   return (
     <div id="home" className="home-section d-flex justify-content-center ps-3 pe-3">
-      {/* <div>
-        <p className="p-square-brack">[ ]</p>
-      </div> */}
       <div>
         <p className="p-my-name">Raiza De Guzman</p>
-        {/* <p className="p-my-title">Full-Stack Web Developer_</p> */}
         <p className="p-my-title">
           <Typewriter options={{
             strings: ["Full-Stack Web Developer", "Hello and welcome"],
