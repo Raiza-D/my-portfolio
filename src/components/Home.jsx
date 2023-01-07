bac385f29136b9b0dffac6d9b0b487665d589704
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-06 23:55:00 -0800

    Remove emoji within Hello welcome string.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
         <p className="p-my-name">Raiza De Guzman</p>
         <p className="p-my-title">
           <Typewriter options={{
-            strings: ["Full-Stack Web Developer", "Hello + welcome 🤓"],
+            strings: ["Full-Stack Web Developer", "Hello + welcome"],
             autoStart: true,
             loop: true,
             delay: 90
