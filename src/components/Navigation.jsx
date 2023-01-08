0d2835c3c2d914b161a679c6c75401945d9ab71a
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-07 18:08:52 -0800

    Add Navbar Toggle and Navbar Collapse to make Navbar responsive.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -15,12 +15,15 @@ export default function Navigation() {
             <img src={myLogo} alt="rdg" width="100px" height="40px" />
           </Nav.Link>
         </Navbar.Brand>
-        <Nav>
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about-me">About Me</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about-me">About Me</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
