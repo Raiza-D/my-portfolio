b5c2b62e1409bde1c4bb9bc327b66d413444583a
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-07 17:19:44 -0800

    Edit paragraph text.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
     <div id="contact" className="contact">
       <Container className="pt-3">
         <h3 className="pb-3">Contact</h3>
-        <p>Connect with me on these platforms:</p>
+        <p>I'm always open to meeting new people in the tech scene and the community beyond! Connect with me on these platforms:</p>
         <div className="d-flex justify-content-evenly align-items-center pt-5">
           <a
             href="https://www.linkedin.com/in/raiza-d/"
